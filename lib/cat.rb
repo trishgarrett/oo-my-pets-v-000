0d51af7149503e3b0975a939b94e5ca290fc09af
Trisha Garrett <trisha.garrett@gmail.com>
2018-05-01 01:51:20 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,9 @@
 class Cat
-  # code goes here
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,7 @@
 class Dog
-  # code goes here
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end  
 end
