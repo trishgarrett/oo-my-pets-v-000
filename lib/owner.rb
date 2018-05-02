5b71e8575d49f1d36db748de1e239b8d9c2763ca
Trisha Garrett <trisha.garrett@gmail.com>
2018-05-02 01:12:02 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,31 @@
+require 'pry'
+
 class Owner
+  attr_accessor :name, :pets
+  attr_reader :species
+
   @@all = []
 
-  def all
+  def initialize(pets)
+    @species = "human"
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+    @name = name
+  end
+
+  def self.all
     @@all
-  end  
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
 end
