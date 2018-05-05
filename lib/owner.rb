29fe2fba8ec77809374551049042c6829fbc965b
Trisha Garrett <trisha.garrett@gmail.com>
2018-05-05 02:23:18 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -82,7 +82,7 @@ class Owner
     def list_pets
       pets.each do|type, pet_array|
         pet_array.each do|pet|
-          #binding.pry
+          binding.pry
 
           "I have #{pet_array.length} fish, #{pet_array.length} dog(s), and #{pet_array.length} cat(s)."
         end
