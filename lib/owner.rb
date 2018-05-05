cc31b5353cd407ab1572ccf51b12638b2c532f37
Trisha Garrett <trisha.garrett@gmail.com>
2018-05-05 02:49:07 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,7 +29,57 @@ class Owner
     "I am a #{@species}."
   end
 
-  def buy_fish()
-
+  def buy_fish(name)
+    pets[:fishes] << Fish.new(name)
   end
+
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    #binding.pry
+    pets[:dogs].each do|dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    pets[:cats].each do|cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    pets[:fishes].each do|fish|
+      fish.mood = "happy"
+    end
+  end
+
+
+    def greeting(name, options = {})
+      if options.empty?
+        puts "Hi, my name is #{name}"
+      else
+        puts "Hi, my name is #{name} and I'm #{options[:age]}" +
+             " years old and I live in #{options[:city]}."
+      end
+    end
+
+    def sell_pets
+      pets.each do|type, pet_array|
+        pet_array.each do|pet|
+          pet.mood = "nervous"
+        end
+        pet_array.clear
+      end
+    end
+
+    def list_pets
+      "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+    end
 end
